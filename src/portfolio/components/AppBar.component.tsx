7186d24f49ce6ba8cbc1d33393a4d0489d89cea7
Pablo <pnunfe@gmail.com>
2023-09-26 10:04:31 +0200

    update exp

--- src/portfolio/components/AppBar.component.tsx
+++ src/portfolio/components/AppBar.component.tsx
@@ -7,48 +7,53 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { grey, blue } from '@mui/material/colors';
 
 const ResponsiveAppBar = () => {
 
+    /// Todo lo comentado aquí es para ocultar la experiencia laboral. 
+
     const theme = useTheme()
-    const { page, setPage } = useConfigAppContext()
-    const { setLang, w, lang: currentLang } = useLangContext()
+    // const { page, setPage } = useConfigAppContext()
+    // La siguiente variable necesita añadir la props 'w'.
+    const { setLang, lang: currentLang } = useLangContext()
     const { toggleTheme, typeTheme } = useConfigAppContext()
 
-    const pages = [
-        {
-            label: page === 'Portfolio' ? w('experience') : w('portfolio'),
-            type: 'internal',
-            // icon: page === 'Portfolio' ? <WorkHistoryIcon sx={{ verticalAlign: 'middle', mr: 1 }} /> : <AccountCircleIcon sx={{ verticalAlign: 'middle', mr: 1 }} />,
-            url: page === 'Portfolio' ? 'Experience' : 'Portfolio'
-        },
+    // const pages = [
+    //     {
+    //         label: page === 'Portfolio' ? w('experience') : w('portfolio'),
+    //         type: 'internal',
+    //         // icon: page === 'Portfolio' ? <WorkHistoryIcon sx={{ verticalAlign: 'middle', mr: 1 }} /> : <AccountCircleIcon sx={{ verticalAlign: 'middle', mr: 1 }} />,
+    //         url: page === 'Portfolio' ? 'Experience' : 'Portfolio'
+    //     },
 
-    ];
+    // ];
 
     const langs = ['ENG', 'ESP',];
 
-    const handleCloseNavMenu = (type: string, url: string) => {
 
-        switch (type) {
-            case 'internal':
-                setPage(url === 'Portfolio' ? 'Portfolio' : 'Experience')
-                break;
-            case 'external':
-                window.open(url, "_blank", "noreferrer");
-                break;
-            default:
-                break;
-        }
+    // HandleClose para el botón de cambiar Experiencia por portafolio
+    // const handleCloseNavMenu = (type: string, url: string) => {
+
+    //     switch (type) {
+    //         case 'internal':
+    //             setPage(url === 'Portfolio' ? 'Portfolio' : 'Experience')
+    //             break;
+    //         case 'external':
+    //             window.open(url, "_blank", "noreferrer");
+    //             break;
+    //         default:
+    //             break;
+    //     }
 
 
-    };
+    // };
 
     const handleCloseLangs = (event: SelectChangeEvent) => {
         setLang(event.target.value === 'ENG' ? 'ENG' : 'ESP');
@@ -117,9 +122,6 @@ const ResponsiveAppBar = () => {
 
                             </Select>
                         </FormControl>
-
-
-
                     </Box>
                 </Toolbar>
             </Container>
